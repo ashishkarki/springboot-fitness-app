1b7021fb27922722ff59db75dc1d753ab18e5fb4
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2025-06-16 19:52:37 +1000

    Enable and assert context load tests

--- activityservice/src/test/java/com/fitness/activityservice/ActivityserviceApplicationTests.java
+++ activityservice/src/test/java/com/fitness/activityservice/ActivityserviceApplicationTests.java
@@ -1,12 +1,21 @@
 package com.fitness.activityservice;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ActivityserviceApplicationTests {
 
-  // @Test
-  // void contextLoads() {
-  // }
+  @Autowired
+  private ApplicationContext context;
+
+  @Test
+  void contextLoads() {
+    assertThat(context).isNotNull();
+  }
 
 }
--- userservice/src/test/java/com/fitness/userservice/UserserviceApplicationTests.java
+++ userservice/src/test/java/com/fitness/userservice/UserserviceApplicationTests.java
@@ -1,12 +1,21 @@
 package com.fitness.userservice;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UserserviceApplicationTests {
 
-  // @Test
-  // void contextLoads() {
-  // }
+  @Autowired
+  private ApplicationContext context;
+
+  @Test
+  void contextLoads() {
+    assertThat(context).isNotNull();
+  }
 
 }
