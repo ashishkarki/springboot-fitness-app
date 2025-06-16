afb11f379857b881c8e5d3c514bd197ca2820ea9
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2025-06-16 19:49:08 +1000

    Use logger in SecurityConfig

--- core/src/main/java/com/fitness/core/config/SecurityConfig.java
+++ core/src/main/java/com/fitness/core/config/SecurityConfig.java
@@ -3,13 +3,15 @@ package com.fitness.core.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Configuration
 public class SecurityConfig {
 
   @Bean
   public BCryptPasswordEncoder passwordEncoder() {
-    System.out.println(">> Creating BCryptPasswordEncoder bean from SecurityConfig (core module)");
+    log.debug("Creating BCryptPasswordEncoder bean from SecurityConfig (core module)");
     return new BCryptPasswordEncoder();
   }
 }
